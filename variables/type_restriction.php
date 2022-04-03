439cf7628d465e9ac26a69bf6db73487b3e27240
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-04-03 20:25:15 +0300

    Глава 5: базовые типы

---
+++ variables/type_restriction.php
@@ -0,0 +1,9 @@
+<?php
+$first = 1;
+$second = 2;
+echo $first + $second; // 3
+
+$greeting = 'Hello, ';
+$subject = 'PHP!';
+echo $greeting . $subject; // Hello, PHP!
+echo $greeting + $subject; // Fatal error: Uncaught TypeError: Unsupported operand types: string + string
