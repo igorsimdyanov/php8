446e2ab0ec7ad0c2db2d48ca75c9e0511e3ec10d
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-04-14 19:55:18 +0300

    Глава 6: предопределенные классы

---
+++ classes/datetime.php
@@ -0,0 +1,3 @@
+<?php
+$date = new DateTime();
+echo $date->format('d-m-Y H:i:s');
---
+++ classes/datetime_type.php
@@ -0,0 +1,4 @@
+<?php
+$date = new DateTime();
+echo gettype($date); // object
+echo get_class($date); // DateTime
