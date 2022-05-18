79048ec3b6395666d1bef523353abc99410053b2
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-05-18 10:03:21 +0300

    Глава 28: регулярные выражения (вычитка)

---
+++ regexp/ex03.php
@@ -0,0 +1,8 @@
+<?php
+$text = 'Привет от somebody@mail.ru, а также от other@mail.ru!';
+$html = preg_replace(
+  '/(\S+)@([a-z0-9.]+)/is',     // найти все E-mail
+  '<a href="mailto:$0">$0</a>', // заменить их по шаблону
+  $text                         // искать в $text
+);
+echo $html;
