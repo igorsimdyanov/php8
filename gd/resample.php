f38be63f53b8cb86eeabe67b5dad27d1c5dcca40
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-06-09 13:51:30 +0300

    Глава 43: черновик

---
+++ gd/resample.php
@@ -0,0 +1,9 @@
+<?php
+$from = imageCreateFromJpeg('sample2.jpg');
+$to   = imageCreateTrueColor(2000, 2000);
+imageCopyResampled(
+    $to, $from, 0, 0, 0, 0, imageSX($to), imageSY($to),
+    imageSX($from), imageSY($from)
+);
+header('Content-type: image/jpeg');
+imageJpeg($to);
