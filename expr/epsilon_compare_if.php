d7faa1b3d4bfa4ac93d5657f4e637adf911fab0f
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-07-19 09:41:09 +0300

    Глава 8: корректура

--- expr/epsilon_compare_if.php
+++ expr/epsilon_compare_if.php
@@ -4,7 +4,7 @@ define('FLOAT_EPSILON', 0.000_000_000_01);
 $fst = 4 / 3 - 1;
 $snd = 1 / 3;
 
-if(abs($fst - $snd) < FLOAT_EPSILON) {
+if (abs($fst - $snd) < FLOAT_EPSILON) {
     echo 'Числа равны';
 } else {
     echo 'Числа не равны';
