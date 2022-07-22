484dbc7b4a078ba6e6b9c7a253e09cd7a8326f1d
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-07-22 22:52:42 +0300

    Глава 16: корректура

--- strings/wrong_symbols.php
+++ strings/wrong_symbols.php
@@ -1,5 +1,12 @@
 <?php
 $str = "Некорректный для UTF-8 символ \x80";
-echo 'ENT_IGNORE: ' . htmlspecialchars($str, ENT_IGNORE) . '<br />';
-echo 'ENT_SUBSTITUTE: ' . htmlspecialchars($str, ENT_SUBSTITUTE) . '<br />';
-echo 'ENT_DISALLOWED: ' . htmlspecialchars($str, ENT_DISALLOWED) . '<br />';
+
+echo 'ENT_IGNORE: ' .
+     htmlspecialchars($str, ENT_IGNORE) .
+     '<br />';
+echo 'ENT_SUBSTITUTE: ' .
+     htmlspecialchars($str, ENT_SUBSTITUTE) .
+     '<br />';
+echo 'ENT_DISALLOWED: ' .
+     htmlspecialchars($str, ENT_DISALLOWED) .
+     '<br />';
