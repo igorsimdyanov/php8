b0ae291b92639cd7471944b4faca4f20b78a7994
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-07-25 08:37:38 +0300

    Корректировка оформления в соответствии со стандартами кодирования

--- superblobals/page.php
+++ superblobals/page.php
@@ -1,5 +1,5 @@
 <?php
 echo "<a href='referer.php'>Ссылка на страницу PHP</a><br />";
-if(isset($_SERVER['HTTP_REFERER'])) {
+if (isset($_SERVER['HTTP_REFERER'])) {
     echo $_SERVER['HTTP_REFERER'];
 }
