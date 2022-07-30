fef21897a13df1d0e4ab72f6ba3f49528b3a9b22
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-07-30 10:58:05 +0300

    Глава 27: корректура

--- date/speed.php
+++ date/speed.php
@@ -1,8 +1,8 @@
 <?php
 define('START_TIME', microtime(true));
 
-for($i = 0; $i < 5; $i++) {
-   sleep(1);
+for ($i = 0; $i < 5; $i++) {
+    sleep(1);
 }
 
 printf('Время работы скрипта: %.5f с', microtime(true) - START_TIME);
